f15165e0cc1679b25560e68a87432e7268e978bd
Tanuj Shrivastava <tanujshrivastava@Tanujs-MacBook-Air.local>
2025-06-29 02:09:33 +0530

    Made changes

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const ip = require('ip')
 
 const app = express()
 
-
-
 app.get('/ping', (req, res) => {
-    for(let i = 0; i < 1000000000; i++) {}
     return res.json({
         message: 'pong',
+        serverAddress: ip.address()
+    })
+});
+
+app.get('/heavy', (req, res) => {
+    console.log("looping")
+    for(let i = 0; i < 100000000000; i++) {}
+    return res.json({
+        message: 'heavy',
         serverAddress: ip.address()
     })
 });
